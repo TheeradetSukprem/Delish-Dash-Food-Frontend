a88f00ba2b5698124164e9a7c9f8eb5caf93d7d3
TheeradetSukprem <161497900+TheeradetSukprem@users.noreply.github.com>
2024-08-05 22:55:03 +0700

    feat:day4 create scroll top homepage and create cart page

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,24 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Navbar.css' 
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom';
+import { StoreContext } from '../../context/StoreContext';
 
 const Navbar = ({setShowLogin}) => {
 
 const [menu,setMenu] = useState("Home");
+const {getTotalCartAmount} = useContext(StoreContext)
+
+const handleHomeClick = () => {
+  setMenu("Home");
+  window.scroll({ top: 0, behavior: 'smooth' });
+}
 
   return (
     <div className='navbar'>
-      <img src={assets.logo} alt="" className='logo' />
+      <Link to="/"><img src={assets.logo} alt="" className='logo' /></Link>
       <ul className="navbar-menu">
-        <Link to='/' onClick={() => setMenu("Home")} className={menu === "Home" ? "active" : ""}>Home</Link>
+        <Link to='/' onClick={handleHomeClick} className={menu === "Home" ? "active" : ""}>Home</Link>
         <a href='#explore-menu' onClick={() => setMenu("Menu")} className={menu === "Menu" ? "active" : ""}>Menu</a>
         <a href='#app-download' onClick={() => setMenu("Mobile-app")} className={menu === "Mobile-app" ? "active" : ""}>Mobile-app</a>        
         <a href='#footer' onClick={() => setMenu("Contact-Us")} className={menu === "Contact-Us" ? "active" : ""}>Contact Us</a>      
@@ -19,8 +26,8 @@ const [menu,setMenu] = useState("Home");
         <div className="navbar-right">
             <img src={assets.search_icon} alt="" />
             <div className="navbar-search-icon">
-                <img src={assets.basket_icon} alt="" />
-                <div className='dot'></div>
+                <Link to="/cart"><img src={assets.basket_icon} alt="" /></Link>
+                <div className={getTotalCartAmount() === 0?"" :"dot"}></div>
             </div>
             <button onClick={()=>setShowLogin(true)}>Sign In</button>
         </div>
